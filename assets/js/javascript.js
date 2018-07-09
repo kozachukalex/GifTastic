171c44a1a5a5e3e3db0aab17389094633deece7b
KamiKozy <kamikozytpp@gmail.com>
2018-07-09 17:12:44 -0400

    mostly done, fix a 403 forbidden error yet

---
+++ assets/js/javascript.js
@@ -0,0 +1,48 @@
+$(function () {
+    var animals = ["dog", "cat", "frog", "horse", "cow", "moose", "bird", "skunk", "fish", "turtle", "molerat", "gopher", "hamster"]
+
+
+    function renderButton() {
+        animals.forEach(function makeButton(animal) {
+            name = animal;
+            var $button = $("<button>")
+                .addClass("animal")
+                .attr("data-name", animal)
+                .text(animal);
+            $("#buttonArea").append($button)
+        })
+    }
+
+    function getGifs() {
+        var animal = $(this).attr("data-name")
+
+        var key = "A6kZfOeAwflSh4Truw4K2V9NVzhxJIfw"
+        var queryUrl = "http://api.giphy.com/v1/gifs/search?q="
+            + animal
+            + "&api_key="
+            + key
+            + "+&limit=10"
+            $.ajax({
+                url: queryUrl,
+                method: "GET"
+              }).then(function(response){
+                  $("#gifArea").append(response)
+              })
+    }
+
+
+    renderButton();
+
+    $("#animalInput").on("click", function addAnimal() {
+        event.preventDefault();
+        var animal = $("#userInput").val()
+        animals.push(animal)
+        $("#buttonArea").html("")
+        renderButton();
+    })
+
+    $(document).on("click",".animal",getGifs)
+
+
+
+})
