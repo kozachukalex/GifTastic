2ab88a84e49bc731e3bcc550570b4e5a26668107
KamiKozy <kamikozytpp@gmail.com>
2018-07-10 03:10:29 -0400

    403 fixed, still images loaded - gif as data....need to create pause/play

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,6 @@
 $(function () {
     var animals = ["dog", "cat", "frog", "horse", "cow", "moose", "bird", "skunk", "fish", "turtle", "molerat", "gopher", "hamster"]
-
+    var gifPlaying = true;
 
     function renderButton() {
         animals.forEach(function makeButton(animal) {
@@ -17,21 +17,33 @@ $(function () {
         var animal = $(this).attr("data-name")
 
         var key = "A6kZfOeAwflSh4Truw4K2V9NVzhxJIfw"
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?q="
+        var queryUrl = "http://api.giphy.com/v1/gifs/search?q="
             + animal
             + "&api_key="
             + key
-            + "+&limit=10"
-            $.ajax({
-                url: queryUrl,
-                method: "GET"
-              }).then(function(response){
-                  $("#gifArea").append(response)
-              })
+            + "&limit=10"
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+
+            $("#gifArea").html("")
+            response.data.forEach(function (gif) {
+                var still = gif.images.fixed_height_still.url
+                var gif = gif.images.fixed_height.url
+                $("#gifArea").append("<img class='gifImage' src='" + still + "' "
+                    + " data-still='" + still + "' "
+                    + " data-animate='" + gif + "' "
+                    + " data-state='still'>")
+            })
+
+        })
     }
 
+    $(".gifImage").on('click', function controlGif() {
+    })
 
-    renderButton();
 
     $("#animalInput").on("click", function addAnimal() {
         event.preventDefault();
@@ -39,9 +51,12 @@ $(function () {
         animals.push(animal)
         $("#buttonArea").html("")
         renderButton();
+        $("#userInput").val("")
     })
 
-    $(document).on("click",".animal",getGifs)
+    renderButton();
+
+    $(document).on("click", ".animal", getGifs)
 
 
 
